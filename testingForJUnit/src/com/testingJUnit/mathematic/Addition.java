215d44cf657bb93333e32bd66bf29eacd14ad72c
User <User@10.41.53.81>
2018-07-21 12:00:35 +0800

    add comment on data type

--- testingForJUnit/src/com/testingJUnit/mathematic/Addition.java
+++ testingForJUnit/src/com/testingJUnit/mathematic/Addition.java
@@ -1,7 +1,7 @@
 package com.testingJUnit.mathematic;
 
 public interface Addition {
-
+	//define data type as long
 	long addition(long... operands);
 	
 }
--- testingForJUnit/src/com/testingJUnit/mathematic/Division.java
+++ testingForJUnit/src/com/testingJUnit/mathematic/Division.java
@@ -1,5 +1,6 @@
 package com.testingJUnit.mathematic;
 
 public interface Division {
+	//define data type as long
 	long divide2(long... operands);
 }
