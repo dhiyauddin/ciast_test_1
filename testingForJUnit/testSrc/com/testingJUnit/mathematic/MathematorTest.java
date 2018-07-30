589bfbd66430696477bd28458e613ec4015e90b7
MEKATRONIK <MEKATRONIK@10.41.51.168>
2018-07-30 15:57:17 +0800

    change value from 10 to 5 in mekatronik lab

--- testingForJUnit/testSrc/com/testingJUnit/mathematic/MathematorTest.java
+++ testingForJUnit/testSrc/com/testingJUnit/mathematic/MathematorTest.java
@@ -17,10 +17,10 @@ public class MathematorTest {
 	@Test
 	public void testSubstraction() {
 		// java substract it down!
-		long result = 100 - 56 - (-12) - 10;
+		long result = 100 - 56 - (-12) - 5;
 		
 		//compare with Mathemator;
-		assertEquals(result , classUnderTest.substraction(100, 56, -12, 10));
+		assertEquals(result , classUnderTest.substraction(100, 56, -12, 5));
 		
 		result = 0;
 		assertEquals(result, classUnderTest.substraction(0));
